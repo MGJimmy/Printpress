46787b7d29b616b21337af9c0c8ce35ffedfb359
Mohamed Gamal <jemi.gamal0112@gmail.com>
2024-12-25 00:22:32 +0200

    Angular: Init sturcture

---
+++ Printpress_Angular/src/app/shared/components/test-shared/test-shared.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-test-shared',
+  standalone: true,
+  imports: [],
+  templateUrl: './test-shared.component.html',
+  styleUrl: './test-shared.component.css'
+})
+export class TestSharedComponent {
+
+}
