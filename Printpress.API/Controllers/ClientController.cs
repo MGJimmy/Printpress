766ea92f2dc28e721d54f01ba7d385eac3b81d56
Rashad Hussein <Rashad.Hussein@linkdev.com>
2024-12-29 20:29:20 +0200

    edit clientController to use class new style

--- Printpress.API/Controllers/ClientController.cs
+++ Printpress.API/Controllers/ClientController.cs
@@ -1,27 +1,18 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Printpress.Application;
+namespace Printpress.API.Controllers;
 
-namespace Printpress.API.Controllers
+
+
+[Route("api/[controller]")]
+[ApiController]
+public class ClientController(IClientService _IClientService) : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class ClientController : ControllerBase
+    [HttpGet]
+    [Route("getClientById")]
+    public async Task<IActionResult> GetClientById(int id)
     {
-        private readonly IClientService _IClientService;
-
-        public ClientController(IClientService clientService)
-        {
-            _IClientService = clientService;
-        }
-
-
-        [HttpGet]
-        [Route("getClientById")]
-        public async Task<IActionResult> GetClientById(int id)
-        {
-            var result =await _IClientService.GetClientById(id);
-            return Ok(result);
-        }
+        var result = await _IClientService.GetClientById(id);
+        return Ok(result);
     }
 }
