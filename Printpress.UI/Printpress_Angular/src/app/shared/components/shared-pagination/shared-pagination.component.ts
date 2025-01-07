05aa8b1dd53609740d34cf4444364ec67fdfe982
mohamed.shukry <mohamed.shukry@nt-me.com>
2025-01-07 19:29:57 +0200

    shared pagination component

---
+++ Printpress.UI/Printpress_Angular/src/app/shared/components/shared-pagination/shared-pagination.component.ts
@@ -0,0 +1,27 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { MatPaginatorModule, PageEvent } from '@angular/material/paginator';
+
+@Component({
+  selector: 'app-shared-pagination',
+  standalone: true,
+  imports: [MatPaginatorModule],
+  templateUrl: './shared-pagination.component.html',
+  styleUrl: './shared-pagination.component.css'
+})
+export class SharedPaginationComponent {
+  @Input() totalItems!: number;
+  @Output() onPageChange: any = new EventEmitter();
+
+  protected pageSizeOptions: number[];
+  protected showFirstLastButtons: boolean;
+
+  constructor() {
+    this.pageSizeOptions = [5, 10, 25, 100];
+    this.showFirstLastButtons = true;
+  }
+
+  onPageChangeClick(event: PageEvent) {
+    this.onPageChange.emit(event);
+  }
+  
+}
