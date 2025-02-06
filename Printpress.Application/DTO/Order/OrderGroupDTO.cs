8e6fc94eba213881ee69a1f945468d10978fad85
mohamed shukry <mohamed.shukry412@gmail.com>
2025-02-07 01:29:29 +0200

    change object state to string

--- Printpress.Application/DTO/Order/OrderGroupDTO.cs
+++ Printpress.Application/DTO/Order/OrderGroupDTO.cs
@@ -2,13 +2,13 @@
 
 namespace Printpress.Application
 {
-    public class OrderGroupDTO: IObjectState
+    public class OrderGroupDTO //: IObjectState
     {
         public int Id { get; set; }
         public string Name { get; set; }
         public int OrderId { get; set; }
         public List<OrderGroupServiceDTO> OrderGroupServices { get; set; }
         public List<ItemDTO> Items { get; set; }
-        public ObjectState ObjectState { get; set; }
+        public string ObjectState { get; set; }
     }
 }
