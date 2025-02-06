23b9446e975122e248187ba23675f76ea7bce512
mohamed shukry <mohamed.shukry412@gmail.com>
2025-02-07 01:25:50 +0200

    BackEnd Insert API: DTOs & Mappers For Order Aggreagates

---
+++ Printpress.Application/DTO/Order/ItemDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace Printpress.Application
+{
+    public class ItemDTO //:IObjectState
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+        public int Price { get; set; }
+        public int GroupId { get; set; }
+        public string ObjectState { get; set; }
+
+    }
+}
---
+++ Printpress.Application/Mappers/ItemMapper.cs
@@ -0,0 +1,24 @@
+﻿using Printpress.Domain.Entities;
+
+namespace Printpress.Application
+{
+    public class ItemMapper : BaseMapper<Item, ItemDTO>
+    {
+        public override Item MapFromDestinationToSource(ItemDTO destinationEntity)
+        {
+            return new Item
+            {
+                Id = destinationEntity.Id,
+                Name = destinationEntity.Name,
+                OrderGroupId = destinationEntity.GroupId,
+                Price = destinationEntity.Price,
+                Quantity = destinationEntity.Quantity
+            };
+        }
+
+        public override ItemDTO MapFromSourceToDestination(Item sourceEntity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
