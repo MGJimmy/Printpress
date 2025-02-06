a5bb596e952e1833cbe5bf86282be37fe1e42c73
mohamed shukry <mohamed.shukry412@gmail.com>
2025-02-07 01:25:26 +0200

    BackEnd Insert API: DTOs & Mappers For Order Aggreagates

---
+++ Printpress.Application/DTO/Order/OrderGroupDTO.cs
@@ -0,0 +1,14 @@
+﻿using Printpress.Domain.Entities;
+
+namespace Printpress.Application
+{
+    public class OrderGroupDTO: IObjectState
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int OrderId { get; set; }
+        public List<OrderGroupServiceDTO> OrderGroupServices { get; set; }
+        public List<ItemDTO> Items { get; set; }
+        public ObjectState ObjectState { get; set; }
+    }
+}
---
+++ Printpress.Application/Mappers/OrderGroupMapper.cs
@@ -0,0 +1,27 @@
+﻿using Printpress.Domain.Entities;
+
+namespace Printpress.Application
+{
+    public class OrderGroupMapper(ItemMapper _itemMapper, GroupServiceMapper _groupServiceMapper) : BaseMapper<OrderGroup, OrderGroupDTO>
+    {
+        public override OrderGroup MapFromDestinationToSource(OrderGroupDTO destinationEntity)
+        {
+            var group = new OrderGroup
+            {
+                Id = destinationEntity.Id,
+                Name = destinationEntity.Name,
+                OrderId = destinationEntity.OrderId
+            };
+
+            group.Items = _itemMapper.MapFromDestinationToSource(destinationEntity.Items);
+            group.Services = _groupServiceMapper.MapFromDestinationToSource(destinationEntity.OrderGroupServices);
+
+            return group;
+        }
+
+        public override OrderGroupDTO MapFromSourceToDestination(OrderGroup sourceEntity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
