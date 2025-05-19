45621b405f61ceae617d16192b93088aa51e9dd4
Mohamed Gamal <jemi.gamal0112@gmail.com>
2025-05-19 14:38:46 +0300

    add NLog logger

--- Printpress.API/Program.cs
+++ Printpress.API/Program.cs
@@ -4,12 +4,16 @@
  * Rashad Elhashmi , Mohamed Gamal , Mohamed Shokry
  **************************************************************************/
 
+using NLog.Web;
 using Printpress.API;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddApplicationServices(builder.Configuration);
 
+// Clear default providers and use NLog
+builder.Logging.ClearProviders();
+builder.Host.UseNLog();
 
 var app = builder.Build();
 
