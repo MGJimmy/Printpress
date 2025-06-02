b504908aa39a7234012364e29760051704b50569
Mohamed Gamal <jemi.gamal0112@gmail.com>
2025-06-02 04:15:01 +0300

    add dbContext factory to used while generate ef DB scripts from migrations

---
+++ Printpress.Infrastructure/Context/ApplicationDbContextFactory.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace Printpress.Infrastructure
+{
+    public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            var basePath = Path.Combine(Directory.GetCurrentDirectory(), "..", "Printpress.API");
+
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(basePath) 
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+
+            optionsBuilder.UseNpgsql( // or UseSqlServer / UseSqlite, etc.
+                configuration.GetConnectionString("DefaultConnection"));
+
+            return new ApplicationDbContext(optionsBuilder.Options);
+        }
+    }
+}
---
+++ UserService/Context/UserDbContextFactory.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace UserService.Presistance
+{
+    public class UserDbContextFactory : IDesignTimeDbContextFactory<UserDbContext>
+    {
+        public UserDbContext CreateDbContext(string[] args)
+        {
+            var basePath = Path.Combine(Directory.GetCurrentDirectory(), "..", "Printpress.API");
+
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(basePath) 
+                .AddJsonFile("appsettings.json", optional: false)
+                .Build();
+
+            var optionsBuilder = new DbContextOptionsBuilder<UserDbContext>();
+            var connectionString = configuration.GetConnectionString("UserConnectionString");
+            optionsBuilder.UseNpgsql(connectionString);
+
+            return new UserDbContext(optionsBuilder.Options);
+        }
+    }
+}
