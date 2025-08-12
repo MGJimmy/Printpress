c5f49256fa2140545b136a650599c2839c880368
Mohamed Gamal <jemi.gamal0112@gmail.com>
2025-08-12 23:27:34 +0300

    Angular: fix mapping service category name

--- Printpress.UI/Printpress_Angular/src/app/features/setup/models/Mappers/service-category-arabic.map.ts
+++ Printpress.UI/Printpress_Angular/src/app/features/setup/models/Mappers/service-category-arabic.map.ts
@@ -3,7 +3,7 @@ import { ServiceCategoryEnum } from "../service-category.enum";
 export const ServiceCategoryArabicMap: Record<ServiceCategoryEnum, string> = {
     [ServiceCategoryEnum.Printing]: 'طباعة',
     [ServiceCategoryEnum.Stapling]: 'تدبيس',
-    [ServiceCategoryEnum.Clueing]: 'لصق',
+    [ServiceCategoryEnum.Clueing]: 'بشر',
     [ServiceCategoryEnum.Cutting]: 'قص',
     [ServiceCategoryEnum.Selling]: 'بيع'
 };
