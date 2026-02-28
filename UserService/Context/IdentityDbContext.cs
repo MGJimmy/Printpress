1f79379f2b52118b87195a9a2f413e9098af4b9e
Mohamed Gamal <jemi.gamal0112@gmail.com>
2026-02-28 22:05:43 +0200

    Add identity db schema

--- UserService/Context/IdentityDbContext.cs
+++ UserService/Context/IdentityDbContext.cs
@@ -19,6 +19,8 @@ namespace UserService.Presistance
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
+            builder.HasDefaultSchema("Identity");
+
             base.OnModelCreating(builder);
 
             builder.Entity<IdentityRole>().HasData(
